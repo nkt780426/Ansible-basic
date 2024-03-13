c350810c70b5ab9df3bbf82216f40f4631275c8e
Võ Nguyễn Hoàng <93706591+nkt780426@users.noreply.github.com>
2024-03-13 22:02:30 +0700

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -5,24 +5,32 @@ sudo apt-get upgrade -y
 sudo apt install ansible -y
 sudo apt install sshpass -y
 
+echo "----------------------------------------------------"
 echo "Path to ssh key for ansible"
 read public_key
 
 echo "----------------------------------------------------"
-echo "Input your account for ansible remote user. You should you the same account with your regular user"
+echo "Input your account for of all user remote user."
+
 echo "Username:"
 read user
 
 echo "Password:"
 read password
-echo "----------------------------------------------------"
 
 if [ -s "hosts.txt" ]; then
     # Đọc từ tệp và thêm khóa cho mỗi máy chủ
     while IFS= read -r line; do
         # Loại bỏ các dấu cách ở đầu và cuối dòng
         host=$(echo "$line" | xargs)
-	sshpass -p $password ssh-copy-id -i $public_key $user@$host
+        # Thêm public key
+            sshpass -p "$password" ssh-copy-id -i "$public_key" "$user@$host"
+        # Kiểm tra mã trả về của lệnh sshpass
+        if [ $? -eq 0 ]; then
+            echo "Successfully added SSH key to $host"
+        else
+            echo "Failed to add SSH key to $host"
+        fi
     done < "hosts.txt"
 else
     echo "hosts.txt doesn't exit or empty."
